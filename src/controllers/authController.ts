3c39a19ad76f3a50d737cdff18215ac608bf356e
axtck <alexandertack@hotmail.be>
2021-11-14 18:04:21 +0100

    Update query

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -74,10 +74,10 @@ export const signup = async (req: Request, res: Response): Promise<void> => {
 
 export const login = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const findUserQuery = `
-        select 
+        SELECT 
             *
-        from users
-        where username = ?
+        FROM users
+        WHERE username = ?
     `;
 
     try {
