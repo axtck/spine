da6102336ad47dd2ff4e401d71b269ecb416c4a5
axtck-bomanj <alexandertack@hotmail.be>
2021-11-24 23:40:03 +0100

    chore(SF1): Update default port use

--- src/config/penv.ts
+++ src/config/penv.ts
@@ -2,7 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    port: process.env.PORT || 3001,
+    port: Number(process.env.PORT) || 3001,
     environment: process.env.NODE_ENV || "development",
     mysqlHost: process.env.MYSQL_HOST,
     mysqlPort: isNaN(Number(process.env.MYSQL_PORT)) ? undefined : Number(process.env.MYSQL_PORT),
