b27e7a2f0e2dd5b37a2d1678a980fb184a8bdbb8
axtck-bomanj <alexandertack@hotmail.be>
2021-12-16 21:39:04 +0100

    chore: remove coverage from config and update test
    use dummy data for tests

--- src/services/tests/AuthService.unit.spec.ts
+++ src/services/tests/AuthService.unit.spec.ts
@@ -1,53 +1,44 @@
 import { InfoError } from "./../../lib/errors/InfoError";
 import { AuthService } from "./../AuthService";
 import sinon from "sinon";
-import { authRepositoryStub } from "../../lib/testing/stubs";
-import { IUserModel } from "../../models/UserModel";
-import { baseUsers, userRoles } from "../../lib/testing/dummies";
-import { IRoleModel } from "../../models/RoleModel";
+import { authRepositoryStub } from "../../lib/testing/stubs/authRepositoryStub";
+import { 
+    dummyUser, dummyAdmin, dummyModerator, 
+    dummyUserRole, dummyAdminRole, dummyModeratorRole
+} from "../../lib/testing/data/dummies";
+
 
 describe("AuthService", () => {
-    const user: IUserModel = baseUsers[0];
-    const admin: IUserModel = baseUsers[1];
-    const moderator: IUserModel = baseUsers[2];
-
-    const userRole: IRoleModel = userRoles[0];
-    const adminRole: IRoleModel = userRoles[1];
-    const moderatorRole: IRoleModel = userRoles[2];
-
-
-    describe("assignRoles", () => {
+        describe("assignRoles", () => {
         const authService: AuthService = new AuthService(authRepositoryStub);
 
         it("should assign user role (user role specified)", async () => {
-            await authService.assignRoles(user.username, [userRole.name]);
-            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, user.id, userRole.id);
+            await authService.assignRoles(dummyUser.username, [dummyUserRole.name]);
+            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, dummyUser.id, dummyUserRole.id);
         });
 
         it("should assign user role (no role in args)", async () => {
-            await authService.assignRoles(user.username, null);
-            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, user.id, userRole.id);
+            await authService.assignRoles(dummyUser.username, null);
+            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, dummyUser.id, dummyUserRole.id);
         });
 
         it("should assign admin role (admin role specified)", async () => {
-            await authService.assignRoles(admin.username, [adminRole.name]);
-            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, admin.id, adminRole.id);
+            await authService.assignRoles(dummyAdmin.username, [dummyAdminRole.name]);
+            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, dummyAdmin.id, dummyAdminRole.id);
         });
 
         it("should assign moderator role (moderator role specified)", async () => {
-            await authService.assignRoles(moderator.username, [moderatorRole.name]);
-            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, moderator.id, moderatorRole.id);
+            await authService.assignRoles(dummyModerator.username, [dummyModeratorRole.name]);
+            sinon.assert.calledWithExactly(authRepositoryStub.createUserRole, dummyModerator.id, dummyModeratorRole.id);
         });
 
         it("should not assign role (no created user found)", async () => {
-            authRepositoryStub.getCreatedUserId.withArgs("").resolves(null);
-            await expect(authService.assignRoles("", [moderatorRole.name]))
+            await expect(authService.assignRoles("", [dummyModeratorRole.name]))
                 .rejects.toThrow(InfoError);
         });
 
         it("should not assign role (no role found)", async () => {
-            authRepositoryStub.getRole.withArgs("").resolves(null);
-            await expect(authService.assignRoles("admin", ["", ""]))
+            await expect(authService.assignRoles("admin", [""]))
                 .rejects.toThrow(InfoError);
         });
     });
